8d2e6800d221a3cb8cb624f77e57cb9da4f855e0
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-05-20 11:05:04 +0300

    test added for calculator methods

--- src/sum.test.js
+++ src/sum.test.js
@@ -19,3 +19,24 @@ test("is input reversed", () => {
 test("reverse multiple word", () => {
   expect(reverseString("learn to test")).toBe("tset ot nrael");
 });
+//test for calculator methods for add,sub multiple and divide for two numbers
+//check input is number only, check the return value is correct
+test("check input is number only", () => {
+  expect(calculator().add("123")).toBeNull();
+  expect(calculator().subtract("123")).toBeNull();
+  expect(calculator().divide("123")).toBeNull();
+  expect(calculator().multiply("123")).toBeNull();
+});
+test("check result for addition method", () => {
+  expect(calculator().add(3, 6)).toEqual(9);
+});
+
+test("check result for subtract method", () => {
+  expect(calculator().add(8, 6)).toEqual(2);
+});
+test("check result for multiplication method", () => {
+  expect(calculator().add(4, 5)).toEqual(20);
+});
+test("check result for division method", () => {
+  expect(calculator().add(15, 3)).toEqual(5);
+});
