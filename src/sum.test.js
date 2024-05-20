3871631a1934c8025d5e137f1e7c7e48aef8cb14
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-05-20 10:33:09 +0300

    test: added for reversestring function

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,11 +1,21 @@
-import { capitalize, sum } from "./functions";
-
+import { capitalize, sum, reverseString } from "./functions";
 test("accept string only", () => {
-  expect(capitalize(123)).toBe(null);
+  expect(capitalize(123)).toBeNull();
 });
 test("capitalize first letter", () => {
   expect(capitalize("you")).toBe("You");
 });
 test("capitalize only the first word", () => {
   expect(capitalize("hey, you")).toBe("Hey, you");
+});
+//write test for reversing string
+//check input is string,check the return value is correct
+test("check input is string", () => {
+  expect(reverseString(123)).toBeNull();
+});
+test("is input reversed", () => {
+  expect(reverseString("you")).toBe("uoy");
+});
+test("reverse multiple word", () => {
+  expect(reverseString("learn to test")).toBe("tset ot nrael");
 });
