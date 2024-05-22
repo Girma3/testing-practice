68c38b5714b05ebf513b9de4611f7a17aae81780
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-05-22 10:01:31 +0300

    old test file  removed

--- try.js
+++
@@ -1,7 +0,0 @@
-function capitalize(str){
-    let arr = [...str];
-    let firstLetter = arr[0].toUpperCase();
-    let result= firstLetter + arr.slice(1).join("")
-    return result
-}
-capitalize('you')
